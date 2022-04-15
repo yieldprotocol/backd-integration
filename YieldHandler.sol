bcb2ed3e31b1d6b647f29a571c8dde0bb6e0f2ff
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2022-04-14 21:07:10 -0500

    fix: correct logic in topUp function

--- YieldHandler.sol
+++ YieldHandler.sol
@@ -6,14 +6,17 @@ import "@yield-protocol/vault-interfaces/DataTypes.sol";
 import "@yield-protocol/vault-interfaces/ICauldron.sol";
 import "@yield-protocol/vault-interfaces/ILadle.sol";
 import "@yield-protocol/utils-v2/contracts/cast/CastU256I128.sol";
+import "@yield-protocol/utils-v2/contracts/token/IERC20.sol";
 
 
 contract YieldHandler is ITopUpHandler {
     using CastU256I128 for uint256;
 
+    DataTypes.Vault vault;
     ICauldron cauldron;
     ILadle ladle;
-    DataTypes.Vault vault;
+    IJoin join;
+    IERC20 collateral;
 
     constructor(ICauldron cauldronAddress, ILadle ladleAddress) {
         cauldron = cauldronAddress;
@@ -25,8 +28,8 @@ contract YieldHandler is ITopUpHandler {
      * This is designed to be called using delegatecall and should therefore
      * not assume that storage will be available
      *
-     * @param account account to be topped up
-     * @param underlying underlying currency to be used for top up
+     * @param account account to be topped up (vaultId)
+     * @param underlying underlying currency to be used for top up (collateral)
      * @param amount amount to be topped up
      * @return true if the top up succeeded and false otherwise
      */
@@ -35,14 +38,14 @@ contract YieldHandler is ITopUpHandler {
         bytes32 account,
         address underlying,
         uint256 amount,
-        bytes memory vaultId
+        bytes memory extra
     ) external returns (bool) {
-        vault = cauldron.vaults(bytes12(vaultId));
-        require(address(uint160(bytes20(account))) == vault.owner, "Wrong vault!");
-        bytes6 seriesId = cauldron.vaults(bytes12(vaultId)).seriesId;
-        bytes6 baseId = cauldron.series(seriesId).baseId;
-        require(underlying == cauldron.assets(baseId), "Wrong underlying!");
-        ladle.pour(vault.seriesId, address(0), amount.i128(), 0);
+        vault = cauldron.vaults(bytes12(account));
+        bytes6 baseId = cauldron.series(vault.seriesId).baseId;
+        require(underlying == cauldron.assets(baseId), "Underlying not found!");
+        join = ladle.joins(vault.ilkId);
+        IERC20(underlying).transfer(address(join), amount);
+        ladle.pour(bytes12(account), address(0), amount.i128(), 0);
         return true;
     }
 
@@ -53,14 +56,7 @@ contract YieldHandler is ITopUpHandler {
      * @param account account for which to get the factor
      */
     function getUserFactor(bytes32 account) external view returns (uint256) {
-        require(address(account) == vault.owner, "Wrong vault!");
-        // TODO: In Yield, if `level` >= 0 then the vault is collateralized.
-        // For now, it seems that we can just add 1 to cauldron.level and backd would be happy,
-        // But it might be interesting to check with them how they use this health factor. It might
-        // be better UX if we convert from our `level`, which returns the value of the collateral in
-        // underlying terms minus the debt. We could instead return the proportion of the value of the
-        // collateral in underlying terms, divided by the value of the debt, as a fixed point number.
-        return uint256(cauldron.level(vault.seriesId)) + 1;
+        return uint256(cauldron.level(bytes12(account))) + 1;
     }
 
 }
